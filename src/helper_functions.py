a97ace928f0c572ed501f86f659acb82e2e2ba86
Raja Shekar <rajashekarb.dev@gmail.com>
2024-10-30 08:11:22 +0000

    minor changes

--- src/helper_functions.py
+++
@@ -1,8 +0,0 @@
-def format_text(input: str) -> str:
-    """
-    Performs text formatting and returns formatted text
-    """
-    cleaned_text = input.replace("hello world", "Hello World").strip()
-
-    return cleaned_text
-
